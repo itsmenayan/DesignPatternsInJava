e6401a4dbf01b83b30da59f8ccc291c3e2228755
Nayan <nayana@mindfiresolutions.com>
2018-10-30 10:49:54 +0530

    Re-adding Abstract Factory Project

---
+++ AbstractFactory/src/com/abstractfactory/Cat.java
@@ -0,0 +1,10 @@
+package com.abstractfactory;
+
+public class Cat implements Animal{
+	
+	@Override
+	public String speak() {
+		return "Meow";
+	}
+
+}
---
+++ AbstractFactory/src/com/abstractfactory/Dog.java
@@ -0,0 +1,10 @@
+package com.abstractfactory;
+
+public class Dog implements Animal{
+	
+	@Override
+	public String speak() {
+		return "Bark";
+	}
+
+}
