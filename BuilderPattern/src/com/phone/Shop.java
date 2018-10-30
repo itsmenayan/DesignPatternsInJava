9e02031857544e245fe6a5b5d155129aa8925f21
Nayan <nayana@mindfiresolutions.com>
2018-10-30 11:13:42 +0530

    Reverting the changes

--- BuilderPattern/src/com/phone/Shop.java
+++ BuilderPattern/src/com/phone/Shop.java
@@ -4,6 +4,9 @@ public class Shop {
 
 	public static void main(String[] args) {
 		
+		/*Phone p = new Phone("IOS",2,"Qualcomm",5.5,3000);
+		System.out.println(p.toString());*/
+		
 		PhoneBuilder pb = new PhoneBuilder();
 		pb.setBattery(3100);
 		pb.setOs("Android");
